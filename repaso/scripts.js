ce8d50323077577fc8e15666e68aef899a08f540
GironB.Rossana <gironb.rossana@gmail.com>
2020-05-26 22:04:12 -0500

    clases cambios

---
+++ repaso/scripts.js
@@ -0,0 +1,48 @@
+// ecm5
+var persona = 'ross'//
+
+// ecm6 int
+let personaslet = 'hello'//
+const personasconst = 'bye'//
+
+/*tipos de variables o tipos de datos
+string Text '' "" ´´//
+NUMBER 1234455//
+BOOLEAN TRUE O FALSE
+let isfun = true
+!+=-
+!-=+
+*/
+let israin = false;
+let isfun = true;
+if (israin) {alert ('lleva sombrilla')}
+else if (!israin){alert ('disfruta tu caminata')}
+
+
+
+// function nombrefuncion(){
+
+// }
+// function nombrefuncion(){
+// return 'algo;'
+// }
+// let perro = {
+    nombre: 'ayudante de santa',
+    color: 'cafe',
+    numerodepatas: 4,
+    tienedueño: true,
+    ladrar(aquien){
+        if (aquien) 
+        {
+            console.log(´guao, guao, ${aquien}! sal de mi casa!´);
+            return 'si ladro';
+        }
+    },
+    console.log('guao, guao, quien eres');
+    return false;
+}//
+console.log(perro.numerodepatas);
+console.log(perro.ladrar('nerys'));
+if (perro.ladrar('gato')=== 'si ladro'{
+    clg ('guaguao, sal de mi casa');
+})
